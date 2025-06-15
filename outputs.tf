5f206ad8c6d46b0e1bd2b815c200665bd6a03b26
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-06-15 16:59:26 -0400

    Fixed TFLint issues, added description

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,20 @@
 # Define output values for later reference
 output "resource_group_name" {
+  description = "The name of the Azure Resource Group"
   value = azurerm_resource_group.rg.name
 }
 
 output "vm_name" {
+  description = "The name of the virtual machine"
   value = azurerm_linux_virtual_machine.webserver.name
 }
 
 output "nic_name" {
+  description = "The name of the network interface"
   value = azurerm_network_interface.webserver.name
 }
 
 output "public_ip" {
+  description = "The public IP address of the VM"
   value = azurerm_linux_virtual_machine.webserver.public_ip_address
 }
